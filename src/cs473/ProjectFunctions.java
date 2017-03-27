577f4d3f2e0f035cc148af3b73dd46adfd4b884c
adam <apk@pluvi.us>
2017-03-27 09:54:25 -0400

    lemonade was a popular drink and it still is

--- src/cs473/ProjectFunctions.java
+++ src/cs473/ProjectFunctions.java
@@ -31,30 +31,35 @@ public class ProjectFunctions {
     public void addAirport(String airportCode, String state, String city) {
         System.out.println(String.format("Adding airport %s\t%s\t%s", airportCode, state, city));
         Airport airport = new Airport(airportCode, city, state);
-        datastore.save(airport);
+        airports.put(airportCode, airport);
+	datastore.save(airport);
     }
 
     public void addPlane(String planeType, int seats) {
         System.out.println(String.format("Adding plane %s\t%d seats", planeType, seats));
         Plane plane = new Plane(planeType, Integer.toString(seats));
+	planes.put(planeType, seats);
         datastore.save(plane);
     }
 
     public void addFlight(String airlineCode, String flightCode, int dayOfWeek, String origAirportCode, String destAirportCode, String planeType) {
         System.out.println(String.format("Adding flight %s\tfrom %s to %s on %d\tplane %s", flightCode, origAirportCode, destAirportCode, dayOfWeek, planeType));
         Flight flight = new Flight(flightCode, airlineCode, origAirportCode, destAirportCode, planeType, "", "", "");
+	flights.put(flightCode, flight);
         datastore.save(flight);
     }
 
     public void addTraveler(int travelerId, String name) {
         System.out.println(String.format("Adding traveller %d\t%s", travelerId, name));
         Traveller traveler = new Traveller(Integer.toString(travelerId), name);
+	travelers.put(travelerId, name);
         datastore.save(traveler);
     }
 
     public void makeReservation(int reservationId, int travelerId, String flightCode, int dayOfWeek, Date date) {
         System.out.println(String.format("Making reservation %d for traveller %d on flight %s for the date %s", reservationId, travelerId, flightCode, date.toString()));
         Reservation reservation = new Reservation(Integer.toString(reservationId), flightCode, Integer.toString(travelerId), "");
+	reservations.put(reservationId, reservation);
         datastore.save(reservation);
     }
 }
